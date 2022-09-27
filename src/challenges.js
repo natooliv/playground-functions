29c49bb48ec9c92d4eac5d785f22c51a2397ab92
Natalia Oliveira <natoliveira7300@hotmail.com>
2022-09-27 15:08:18 -0300

    Desafio 01

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(paramA, paramB) {
+  if (paramA === true && paramB === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
